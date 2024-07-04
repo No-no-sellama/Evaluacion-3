bf03f4774c1e8c4c7fdf9de2b426ac41b64cb89a
Daniel F. Antio <danielflp.82@gmail.com>
2024-07-04 15:22:17 -0400

    subida
    asdsadasd

---
+++ assets/js/script.js
@@ -0,0 +1,88 @@
+// AOS
+AOS.init();
+
+let v1;
+let v2;
+let v3;
+let v4;
+let v5;
+let v6;
+let v7;
+let v8;
+let v9;
+let v10;
+let v11;
+let v12;
+let v13;
+let v14;
+
+
+function abreVentana() {
+    alert('Redireccionando a los productos de pearl jam');
+    v1 = window.open("https://shop.pearljam.com/products/pearl-jam-dark-matter" , "_blank")
+}
+
+function abreVentana2() {
+    alert('Redireccionando a los productos de pearl jam');
+    v2 = window.open("https://www.amazon.com/Gigaton-Pearl-Jam/dp/B083T61W5J/ref=sr_1_1?sr=8-1" , "_blank")
+}
+
+function abreVentana3() {
+    alert('Redireccionando a los productos de pearl jam');
+    v2 = window.open("https://www.amazon.com/Lightning-Bolt-Pearl-Jam/dp/B00DW1WR14/ref=sr_1_1?sr=8-1" , "_blank")
+}
+
+function abreVentana4() {
+    alert('Redireccionando a los productos de pearl jam');
+    v2 = window.open("https://www.amazon.com/BACKSPACER-ltd-reissue/dp/B003K3HCJQ/ref=sr_1_1?sr=8-1" , "_blank")
+}
+
+function abreVentana5() {
+    alert('Redireccionando a los productos de pearl jam');
+    v2 = window.open("https://www.amazon.com/Pearl-Jam/dp/B06ZZQNL71/ref=sr_1_1?sr=8-1" , "_blank")
+}
+
+function abreVentana6() {
+    alert('Redireccionando a los productos de pearl jam');
+    v2 = window.open("https://www.amazon.com/Pearl-Jam-Rearview-Mirror-Bundle/dp/B09XQKJWH5/ref=sr_1_1?sr=8-1" , "_blank")
+}
+
+function abreVentana7() {
+    alert('Redireccionando a los productos de pearl jam');
+    v2 = window.open("https://www.amazon.com/Riot-Act-Pearl-Jam/dp/B06Y6K1VJ8/ref=sr_1_1?sr=8-1" , "_blank")
+}
+
+function abreVentana8() {
+    alert('Redireccionando a los productos de pearl jam');
+    v2 = window.open("https://www.amazon.com/Binaural-Pearl-Jam/dp/B0014YVCDK/ref=sr_1_1?sr=8-1" , "_blank")
+}
+
+function abreVentana9() {
+    alert('Redireccionando a los productos de pearl jam');
+    v2 = window.open("https://www.amazon.com/No-Code-PEARL-JAM/dp/B076XBK493/ref=tmm_acd_swatch_0?_encoding=UTF8&sr=8-1" , "_blank")
+}
+
+function abreVentana10() {
+    alert('Redireccionando a los productos de pearl jam');
+    v2 = window.open("https://www.amazon.com/Vitalogy-Vinyl-Remastered-Pearl-Jam/dp/B004Q3RKH8/ref=sr_1_1?sr=8-1" , "_blank")
+}
+
+function abreVentana11() {
+    alert('Redireccionando a los productos de pearl jam');
+    v2 = window.open("https://www.amazon.com/Yield-Pearl-Jam/dp/B0045DO8LK/ref=tmm_acd_swatch_0?_encoding=UTF8&sr=8-1" , "_blank")
+}
+
+function abreVentana12() {
+    alert('Redireccionando a los productos de pearl jam');
+    v2 = window.open("https://www.amazon.com/Vs-30th-Anniversary-Pearl-Jam/dp/B0CF85KV6Y/ref=sr_1_2?sr=8-2" , "_blank")
+}
+
+function abreVentana13() {
+    alert('Redireccionando a los productos de pearl jam');
+    v2 = window.open("https://www.amazon.com/music/player?ie=UTF8&qid=1333560264&sr=301-3" , "_blank")
+}
+
+function abreVentana14() {
+    alert('Redireccionando a los productos de pearl jam');
+    v2 = window.open("https://www.amazon.com/Ten-Pearl-Jam/dp/B0000027RL/ref=tmm_acd_swatch_0?_encoding=UTF8&sr=8-1" , "_blank")
+}
